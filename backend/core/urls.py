84e4dfb53ab82c232cd7ccca7dbf5b7dd23a783d
Pavel Paranin <pavel@paran.in>
2023-01-02 19:03:45 +0100

    Point of no return

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from .api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
+    path("users/", include("users.urls"))
 ]
